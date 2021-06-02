0e9b618e836d6a5122f65840654eab919855df3f
Mr.T <antoniotierno@gmail.com>
2021-06-02 13:44:12 +0200

    Add files via upload

--- patterns/tmr_v111.py
+++ patterns/tmr_v111.py
@@ -22,6 +22,9 @@ class TmrV111Definition(PatternDefinition):
         super(TmrV111Definition, self).__init__(comp_name, pt_name, comp_n_inputs, modules_f_atoms + [voter_f_atom], PatternType.TMR_V111)
         self._pt_name = pt_name
 
+    def get_dummy_definition(self) -> 'TmrV111Definition':
+        return TmrV111Definition("EMPTY", self.comp_n_inputs, [Symbol("EMPTY_F" + str(idx)) for idx in range(3)], Symbol("EMPTY_F3"))
+
     def create(self, nominal_mod_beh) -> Pattern:
         return TmrV111(self._comp_name, self._pt_name, self._comp_n_inputs, self._modules_f_atoms, self._voter_f_atom, nominal_mod_beh)
 
@@ -40,12 +43,12 @@ class TmrV111Definition(PatternDefinition):
         return self._voter_f_atom
 
 
+
 class TmrV111(Pattern):
     """
     Represent a pattern of type tmr-v111
     """
     n_f_atoms = 4
-
     def __init__(self, comp_name: str, pt_name: str, comp_n_inputs: int, modules_fault_atoms: list, voter_fault_atom: Symbol, nominal_mod_beh: Symbol):
         """
         Creata TMR-V111 pattern
@@ -53,21 +56,23 @@ class TmrV111(Pattern):
         :param comp_n_inputs: number of inputs of the component to which the pattern has to be applied
         :param modules_fault_atoms: list of fault atoms associated to the 3 modules
         :param voter_fault_atom: fault atom associated to the voter
-        :param nominal_mod_beh: nominal behaviour of the modules
+        :param nominal_mod_beh: nominal behavior of the modules
         """
         pattern_name = comp_name + "." + pt_name
-        modules = [FaultyModule(pattern_name + ".M" + str(idx), comp_n_inputs, modules_fault_atoms[idx], nominal_mod_beh) for idx in range(3)]
-        modules_out_ports = []
+        modules = [FaultyModule(pattern_name + ".M" + str(idx), comp_n_inputs, modules_fault_atoms[idx], nominal_mod_beh) for idx in range(3)]        
+        
         # The output of the modules are the inputs of the voter
+        modules_out_ports = []
         for module in modules:
             modules_out_ports.extend(module.output_ports)
         assert len(modules_out_ports) == 3, "[" + pattern_name + "] The voter must have 3 inputs"
         self._voter = Voter(pattern_name + ".V", voter_fault_atom, input_ports=modules_out_ports)
+        
         # Output port of the pattern corresponds to the output port of the voter
         output_ports = self._voter.output_ports
         super(TmrV111, self).__init__(pattern_name, PatternType.TMR_V111, modules_fault_atoms + [voter_fault_atom], modules, output_ports)
 
-        # Define behaviour formula: And of subcomponents behaviours
+        # Define behavior formula: And of subcomponents behaviors
         # Modules
         subcomp_beh_formula = [module.behaviour_formula for module in modules]
         # Voter
@@ -75,12 +80,12 @@ class TmrV111(Pattern):
         self._behaviour_formula = And(subcomp_beh_formula)
 
 
-'''
+
 # Test - Example
 if __name__ == "__main__":
-    tmr = TmrV111("C1", 1, [Symbol("F0"), Symbol("F1"), Symbol("F2")], Symbol("F3"))
+    tmr = TmrV111("C1", "P1", 1, [Symbol("F0"), Symbol("F1"), Symbol("F2")], Symbol("F3"),  Symbol("Beh"))
     print(tmr.behaviour_formula.serialize())
-
+'''
     # Output
     # (((! F0) -> (C1.TMR_V111.M0.o0 = C1.TMR_V111.M0.beh(C1.TMR_V111.M0.i0))) & 
     # ((! F1) -> (C1.TMR_V111.M1.o0 = C1.TMR_V111.M1.beh(C1.TMR_V111.M1.i0))) & 
