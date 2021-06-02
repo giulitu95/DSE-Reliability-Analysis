72855e1b83453de8502065658ba1a394a6afbbbd
Mr.T <antoniotierno@gmail.com>
2021-06-02 17:41:14 +0200

    Add files via upload
    Added various Pattern specifications

--- patterns/pt_spec.py
+++ patterns/pt_spec.py
@@ -1,5 +1,6 @@
-from params import NonFuncParamas
+from params import NonFuncParams
 from patterns import PatternType
+
 
 
 class PatternSpec:
@@ -15,9 +16,196 @@ class PatternSpec:
     def name(self):
         return self._name
 
+'''
+Duplex
+'''
+    
+class DpxSpec(PatternSpec):
+    def __init__(self, name, modules_params: list, comparator_param: NonFuncParams):
+        '''
+        Create a specification of a DPX
+        :param modules_probs: list of length 2 containing the probabilities of the 2 modules
+        :param voter_probs: fault probability of the comparator
+        '''
+        self._modules_params = modules_params
+        self._comparator_params = comparator_param
+        super(DpxSpec, self).__init__(PatternType.DPX_HM, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def comparator_param(self):
+        return self._comparator_param
+    
+
+'''
+TMR
+'''
+            
+class TmrV001Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V001
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 3 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV001Spec, self).__init__(PatternType.TMR_V001, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params
+
+
+class TmrV010Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V010
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 3 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV010Spec, self).__init__(PatternType.TMR_V010, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params
+    
+
+class TmrV011Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V011
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 3 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV011Spec, self).__init__(PatternType.TMR_V011, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params
+        
+
+class TmrV012Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V012
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 2 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV012Spec, self).__init__(PatternType.TMR_V012, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params
+        
+
+class TmrV100Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V100
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 3 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV100Spec, self).__init__(PatternType.TMR_V100, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params
+    
+
+class TmrV101Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V101
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 3 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV101Spec, self).__init__(PatternType.TMR_V101, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params
+    
+
+class TmrV102Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V103
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 2 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV102Spec, self).__init__(PatternType.TMR_V102, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params
+    
+    
+class TmrV110Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V110
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 3 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV110Spec, self).__init__(PatternType.TMR_V110, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params                    
+
 
 class TmrV111Spec(PatternSpec):
-    def __init__(self, name, modules_params: list, voter_param: NonFuncParamas):
+    def __init__(self, name, modules_params: list, voter_param: NonFuncParams):
         '''
         Create a specification of a TMR-V111
         :param modules_probs: list of length 3 containing the probabilities of the 3 modules
@@ -36,4 +224,185 @@ class TmrV111Spec(PatternSpec):
         return self._voter_param
 
 
+class TmrV111x3Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V111
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 3 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV111x3Spec, self).__init__(PatternType.TMR_V111x3, name)
 
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params
+
+
+class TmrV112Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V112
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 2 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV112Spec, self).__init__(PatternType.TMR_V112, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params
+  
+
+class TmrV120Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V120
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 2 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV120Spec, self).__init__(PatternType.TMR_V120, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params  
+  
+
+class TmrV122Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V122
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 2 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV122Spec, self).__init__(PatternType.TMR_V122, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params  
+
+
+class TmrV123Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_params: list):
+        '''
+        Create a specification of a TMR-V123
+        :param modules_probs: list of length 3 containing the probabilities of the 3 modules
+        :param voter_probs: fault probability of the 3 voters
+        '''
+        self._modules_params = modules_params
+        self._voter_params = voter_params
+        super(TmrV123Spec, self).__init__(PatternType.TMR_V123, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_params(self):
+        return self._voter_params  
+
+
+'''
+X-o-o-Y
+'''
+
+class XooY_3oo4Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_param: NonFuncParams):
+        '''
+        Create a specification of a 3-o-o-4
+        :param modules_probs: list of length 4 containing the probabilities of the 4 modules
+        :param voter_probs: fault probability of the voter
+        '''
+        self._modules_params = modules_params
+        self._voter_param = voter_param
+        super(XooY_3oo4Spec, self).__init__(PatternType.XooY_3oo4, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_param(self):
+        return self._voter_param
+
+
+class XooY_3oo5Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_param: NonFuncParams):
+        '''
+        Create a specification of a 3-o-o-5
+        :param modules_probs: list of length 5 containing the probabilities of the 5 modules
+        :param voter_probs: fault probability of the voter
+        '''
+        self._modules_params = modules_params
+        self._voter_param = voter_param
+        super(XooY_3oo5Spec, self).__init__(PatternType.XooY_3oo5, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_param(self):
+        return self._voter_param
+
+
+class XooY_4oo6Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_param: NonFuncParams):
+        '''
+        Create a specification of a 4-o-o-6
+        :param modules_probs: list of length 6 containing the probabilities of the 6 modules
+        :param voter_probs: fault probability of the voter
+        '''
+        self._modules_params = modules_params
+        self._voter_param = voter_param
+        super(XooY_4oo6Spec, self).__init__(PatternType.XooY_4oo6, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_param(self):
+        return self._voter_param
+
+
+class XooY_4oo7Spec(PatternSpec):
+    def __init__(self, name, modules_params: list, voter_param: NonFuncParams):
+        '''
+        Create a specification of a 4-o-o-7
+        :param modules_probs: list of length 7 containing the probabilities of the 7 modules
+        :param voter_probs: fault probability of the voter
+        '''
+        self._modules_params = modules_params
+        self._voter_param = voter_param
+        super(XooY_4oo7Spec, self).__init__(PatternType.XooY_4oo7, name)
+
+    @property
+    def modules_params(self):
+        return self._modules_params
+
+    @property
+    def voter_param(self):
+        return self._voter_param
--- patterns/tmr_v100.py
+++ patterns/tmr_v100.py
@@ -2,15 +2,16 @@ from patterns.pattern import Pattern, PatternType, PatternDefinition
 from components.module import FaultyModule
 from components.voter import Voter
 from pysmt.shortcuts import *
+from networkx.algorithms.centrality.degree_alg import out_degree_centrality
 
 
-class TmrV111Definition(PatternDefinition):
+class TmrV100Definition(PatternDefinition):
     """
-    Definition for pattern TMR-V111
+    Definition for pattern TMR-V100
     """
     def __init__(self, comp_name: str, pt_name: str, comp_n_inputs: int, modules_f_atoms: list, voter_f_atom: Symbol):
         """
-        Create a definition for pattern TMRV111
+        Create a definition for pattern TMRV100
         :param comp_name: name of the component
         :param comp_n_inputs: number of component's inputs
         :param modules_f_atoms: list of fault atoms of the pattern
@@ -19,14 +20,14 @@ class TmrV111Definition(PatternDefinition):
         self._modules_f_atoms = modules_f_atoms
         assert len(modules_f_atoms) == 3, "[" + pt_name + "] pattern has 3 modules, a correct number of fault atoms"
         self._voter_f_atom = voter_f_atom
-        super(TmrV111Definition, self).__init__(comp_name, pt_name, comp_n_inputs, modules_f_atoms + [voter_f_atom], PatternType.TMR_V111)
+        super(TmrV100Definition, self).__init__(comp_name, pt_name, comp_n_inputs, modules_f_atoms + [voter_f_atom], PatternType.TMR_V100)
         self._pt_name = pt_name
 
-    def get_dummy_definition(self) -> 'TmrV111Definition':
-        return TmrV111Definition("EMPTY", self.comp_n_inputs, [Symbol("EMPTY_F" + str(idx)) for idx in range(3)], Symbol("EMPTY_F3"))
+    def get_dummy_definition(self) -> 'TmrV100Definition':
+        return TmrV100Definition("EMPTY", self.comp_n_inputs, [Symbol("EMPTY_F" + str(idx)) for idx in range(3)], Symbol("EMPTY_F3"))
 
     def create(self, nominal_mod_beh) -> Pattern:
-        return TmrV111(self._comp_name, self._pt_name, self._comp_n_inputs, self._modules_f_atoms, self._voter_f_atom, nominal_mod_beh)
+        return TmrV100(self._comp_name, self._pt_name, self._comp_n_inputs, self._modules_f_atoms, self._voter_f_atom, nominal_mod_beh)
 
     @property
     def modules_f_atoms(self) -> list:
@@ -44,14 +45,14 @@ class TmrV111Definition(PatternDefinition):
 
 
 
-class TmrV111(Pattern):
+class TmrV100(Pattern):
     """
-    Represent a pattern of type tmr-v111
+    Represent a pattern of type tmr-v100
     """
     n_f_atoms = 4
     def __init__(self, comp_name: str, pt_name: str, comp_n_inputs: int, modules_fault_atoms: list, voter_fault_atom: Symbol, nominal_mod_beh: Symbol):
         """
-        Creata TMR-V111 pattern
+        Creata TMR-V100 pattern
         :param comp_name: name of the component to which the pattern has to be applied
         :param comp_n_inputs: number of inputs of the component to which the pattern has to be applied
         :param modules_fault_atoms: list of fault atoms associated to the 3 modules
@@ -68,9 +69,13 @@ class TmrV111(Pattern):
         assert len(modules_out_ports) == 3, "[" + pattern_name + "] The voter must have 3 inputs"
         self._voter = Voter(pattern_name + ".V", voter_fault_atom, input_ports=modules_out_ports)
         
-        # Output port of the pattern corresponds to the output port of the voter
-        output_ports = self._voter.output_ports
-        super(TmrV111, self).__init__(pattern_name, PatternType.TMR_V111, modules_fault_atoms + [voter_fault_atom], modules, output_ports)
+        # Output port of the pattern corresponds to the output port of the voter and the output ports of Module 2 and Module 3
+        output_ports = []
+        output_ports.extend(self._voter.output_ports) #1
+        fan_outs=(module[1], module[2]) 
+        output_ports.extend(fan_outs) #0 0
+        assert len(output_ports) == 3, "[" + pattern_name + "] The pattern must have 3 outputs" 
+        super(TmrV100, self).__init__(pattern_name, PatternType.TMR_V100, modules_fault_atoms + [voter_fault_atom], modules, output_ports)
 
         # Define behavior formula: And of subcomponents behaviors
         # Modules
@@ -78,17 +83,3 @@ class TmrV111(Pattern):
         # Voter
         subcomp_beh_formula.append(self._voter.behaviour_formula)
         self._behaviour_formula = And(subcomp_beh_formula)
-
-
-
-# Test - Example
-if __name__ == "__main__":
-    tmr = TmrV111("C1", "P1", 1, [Symbol("F0"), Symbol("F1"), Symbol("F2")], Symbol("F3"),  Symbol("Beh"))
-    print(tmr.behaviour_formula.serialize())
-'''
-    # Output
-    # (((! F0) -> (C1.TMR_V111.M0.o0 = C1.TMR_V111.M0.beh(C1.TMR_V111.M0.i0))) & 
-    # ((! F1) -> (C1.TMR_V111.M1.o0 = C1.TMR_V111.M1.beh(C1.TMR_V111.M1.i0))) & 
-    # ((! F2) -> (C1.TMR_V111.M2.o0 = C1.TMR_V111.M2.beh(C1.TMR_V111.M2.i0))) & 
-    # ((! F3) -> (((C1.TMR_V111.M0.o0 = C1.TMR_V111.M1.o0) | (C1.TMR_V111.M0.o0 = C1.TMR_V111.M2.o0)) ? (C1.TMR_V111.V.o0 = C1.TMR_V111.M0.o0) : ((C1.TMR_V111.M1.o0 = C1.TMR_V111.M2.o0) -> (C1.TMR_V111.V.o0 = C1.TMR_V111.M1.o0)))))
-'''
